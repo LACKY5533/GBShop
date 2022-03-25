562932221aba064b8214a1c3c459fc24e69184d8
LACKY5533 <atrashkov.nikita@mail.ru>
2022-03-25 15:25:37 +0300

    Initial

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by LACKY on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
