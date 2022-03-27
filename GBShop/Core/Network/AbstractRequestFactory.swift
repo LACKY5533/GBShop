24ada91968c2526f79e0ea0cdc5511c7d3e838be
LACKY5533 <atrashkov.nikita@mail.ru>
2022-03-28 00:23:17 +0300

    Login,Logout,SignUp,ChangePersonalUserData classes

---
+++ GBShop/Core/Network/AbstractRequestFactory.swift
@@ -0,0 +1,30 @@
+//
+//  AbstractRequestFactory.swift
+//  GBShop
+//
+//  Created by LACKY on 27.03.2022.
+//
+
+import Foundation
+import Alamofire
+
+protocol AbstractRequestFactory {
+    
+    var errorParser: AbstractErrorParser { get }
+    var sessionManager: Session { get }
+    var queue: DispatchQueue { get }
+    
+    @discardableResult
+    func request<T: Decodable>( request: URLRequestConvertible, completionHandler: @escaping (AFDataResponse<T>) -> Void) -> DataRequest
+}
+
+extension AbstractRequestFactory {
+    
+    @discardableResult
+    public func request<T: Decodable>( request: URLRequestConvertible, completionHandler: @escaping (AFDataResponse<T>) -> Void) -> DataRequest {
+        
+    return sessionManager
+        .request(request)
+        .responseCodable(errorParser: errorParser, queue: queue, completionHandler: completionHandler)
+    }
+}
